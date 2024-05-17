9684c5067799130ddd0667a3bd28f679178dec5b
2DEN58 <60464471+2DEN58@users.noreply.github.com>
2024-05-17 18:05:51 +0100

    Upload Farmacia 20240517

---
+++ Farmacia/src/farmacia/Farmacia.java
@@ -0,0 +1,18 @@
+package farmacia;
+
+/**
+ *
+ * @author Denzel Baysa
+ */
+public class Farmacia {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Formulario1 f1 = new Formulario1();
+        f1.setSize(495, 620);
+        f1.setVisible(true);
+    }
+    
+}
